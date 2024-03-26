01bc51d9b9eff6172c8fc6590f593e805d6c990f
Joel <git@runningant.com>
2024-03-26 12:14:37 +1100

    Changing the way first() works to be more convenient

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,20 @@ export function uuid() {
     return uuidv4();
 }
 
-export function first(array: any[]) {
-    if (!array || array.length <= 0) return undefined;
-    return array[0];
+// if there is an array provided and it has at least 1 element the first is returned
+// otherwise if its not an array but is an object then its returned as-is
+// otherwised undefined
+export function first(array: any) {
+    if (!array) return undefined;
+
+	if (Array.isArray(array)) {
+		if (array.length > 0) return array[0];
+		return undefined;
+	} else if (isObject(array)) {
+		return array;
+	}
+
+    return undefined;
 }
 
 export function toInt(val: any, valDefault?: number) {
