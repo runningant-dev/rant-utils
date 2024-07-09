2e2a474985ad3f9ec6cc4a260ac544a303a9e4a8
Joel <git@runningant.com>
2024-07-09 23:05:33 +1000

    Removing cryptography references

--- js/index.d.ts
+++ js/index.d.ts
@@ -3,6 +3,3 @@ export * from "./colorText";
 export * from "./format";
 export * from "./object";
 export * from "./validate-convert";
-export declare function hash(salt: string, value: string): string;
-export declare function uuid(): string;
-export declare function uuid4(): string;
--- js/index.js
+++ js/index.js
@@ -13,27 +13,9 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uuid4 = exports.uuid = exports.hash = void 0;
-const uuid_1 = require("uuid");
-const crypto_1 = __importDefault(require("crypto"));
 __exportStar(require("./array"), exports);
 __exportStar(require("./colorText"), exports);
 __exportStar(require("./format"), exports);
 __exportStar(require("./object"), exports);
 __exportStar(require("./validate-convert"), exports);
-function hash(salt, value) {
-    return crypto_1.default.createHmac("sha256", salt).update(value).digest("hex");
-}
-exports.hash = hash;
-function uuid() {
-    return (0, uuid_1.v1)();
-}
-exports.uuid = uuid;
-function uuid4() {
-    return (0, uuid_1.v4)();
-}
-exports.uuid4 = uuid4;
